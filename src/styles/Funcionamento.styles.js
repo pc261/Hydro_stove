27ac4b9cdc54771d2f4396f97275ec30c45893b0
Igor Miguel <igormedeirosmiguel@gmail.com>
2024-07-10 20:21:50 -0300

    avanço na pagina do nosso projeto, restando ajuste de detalhes

--- src/styles/Funcionamento.styles.js
+++ src/styles/Funcionamento.styles.js
@@ -55,9 +55,6 @@ export const ImagemContainer = styled.section`
             height: 43.7rem;
         }
     }
-    
-
-
 `
 
 export const Utilizacao = styled.section`  
@@ -75,6 +72,16 @@ export const Utilizacao = styled.section`
     margin-top: 5.5rem;
     margin-bottom: 5.5rem;
 
+    .primeiro {
+    justify-content: center;
+    display: flex;
+    }
+
+    .ultimo {
+    justify-content: center;
+    display: flex;
+    }
+
 
     @media (min-width: 1440px) {
         width: 85%;
@@ -88,5 +95,109 @@ export const Utilizacao = styled.section`
         padding-block: 1.4rem;
 
         font-size: 2.6rem;
+    }
+`
+
+export const Fotos = styled.section`
+    h1 {
+        font-size: 6.4rem;
+        color: #BDBDBD;
+        font-family: "Roboto", sans-serif;
+        font-weight: 300;
+        margin-top: 2rem;
+    }
+
+    h2 {
+        font-size: 6.4rem;
+        color: #333333;
+        font-family: "Roboto", sans-serif;
+        font-weight: bold;
+        margin-bottom: 3rem;
+    }
+
+    div {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        text-align: center;
+    }
+
+    .imagem-mobile{
+        width: 11.12rem;
+        height: 10.31rem;
+        margin: 1rem 2rem 2rem 2rem;
+    }
+
+    p{
+        color: #BDBDBD;
+        font-size: 1.38rem;
+        font-family: "Arimo", sans-serif;
+    }
+
+    @media (min-width: 1440px) {
+        width: 81%;
+        display: block;
+
+        .imagem-mobile{
+        width: 31.44rem;
+        height: 27.31rem;
+        margin: 1rem 2rem 2rem 2rem;
+        }
+
+        p{
+            font-size: 4rem;
+            background-color: #20483D;
+            margin: 1rem;
+
+            padding-inline: 3.8rem;
+            padding-block: 1.4rem;
+        }
+    }
+`
+
+export const Integracao = styled.section`
+    width: 80%;
+
+    h1 {
+        font-size: 5.4rem;
+        color: #BDBDBD;
+        font-family: "Roboto", sans-serif;
+        font-weight: 300;
+        margin-top: 2rem;
+    }
+
+    h2 {
+        font-size: 5.4rem;
+        color: #333333;
+        font-family: "Roboto", sans-serif;
+        font-weight: bold;
+        margin-bottom: 3rem;
+    }
+
+    p {
+        font-size: 1.5rem;
+        color: #FFFFFF;
+        font-family: "Arimo", sans-serif;
+        margin-bottom: 2rem;
+    }
+
+    @media (min-width: 1440px) {
+        width: 81%;
+        display: block;
+
+        .imagem-mobile{
+        width: 31.44rem;
+        height: 27.31rem;
+        margin: 1rem 2rem 2rem 2rem;
+        }
+
+        p{
+            font-size: 2.6rem;
+            background-color: #20483D;
+            margin: 1rem;
+            border-radius: 1rem;
+            padding-inline: 3.8rem;
+            padding-block: 1.4rem;
+        }
     }
 `
