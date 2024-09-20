8d68b965cb5d28610e1b5c7c452d454e3ab04e59
Igor Miguel <igormedeirosmiguel@gmail.com>
2024-09-19 21:28:18 -0300

    ajustes

--- src/pages/Bibliografia.jsx
+++ src/pages/Bibliografia.jsx
@@ -22,7 +22,7 @@ export function Bibliografia() {
         <Referencia>
 
             <div>
-                <h1>Qualidade da água na agricultura e no ambiente</h1>
+                <h1>Qualidade da água na agricultura e no ambiente.</h1>
                 <a target="_blank" href="https://www.bibliotecaagptea.org.br/administracao/educacao/artigos/MANEJO%20DO%20AMBIENTE%20EM%20CULTIVO%20PROTEGIDO.pdf">
                     <p>Ver mais</p>
                     <img src={Seta} alt="" />
@@ -30,7 +30,7 @@ export function Bibliografia() {
             </div>
             <hr />
             <div>
-                <h1>MANEJO DO AMBIENTE EM CULTIVO PROTEGIDO</h1>
+                <h1>MANEJO DO AMBIENTE EM CULTIVO PROTEGIDO.</h1>
                 <a target="_blank" href="https://www.bibliotecaagptea.org.br/administracao/educacao/artigos/MANEJO%20DO%20AMBIENTE%20EM%20CULTIVO%20PROTEGIDO.pdf">
                     <p>Ver mais</p>
                     <img src={Seta} alt="" />
@@ -42,7 +42,7 @@ export function Bibliografia() {
         <Referencia>
 
             <div>
-                <h1>Ciência, tecnologia e inovação na interface entre as áreas de recursos hídricos e saneamento</h1>
+                <h1>Ciência, tecnologia e inovação na interface entre as áreas de recursos hídricos e saneamento.</h1>
                 <a target="_blank" href="https://www.scielo.br/j/esa/a/dQkgWPTQL8mQfGKvMmKnCyC/">
                     <p>Ver mais</p>
                     <img src={Seta} alt="" />
@@ -50,7 +50,7 @@ export function Bibliografia() {
             </div>
             <hr />
             <div>
-                <h1>PROJETO EXPERIMENTAL DE ESTUFA DOMÉSTICA PARA FIM DE CULTIVO ALIMENTÍCIO E MEDICINAL</h1>
+                <h1>PROJETO EXPERIMENTAL DE ESTUFA DOMÉSTICA PARA FIM DE CULTIVO ALIMENTÍCIO E MEDICINAL;</h1>
                 <a target="_blank" href="https://repositorio.usp.br/directbitstream/f4ca1ac2-ab85-42e3-a365-0fd5d48b44e8/EP21001%20Furlan.pdf">
                     <p>Ver mais</p>
                     <img src={Seta} alt="" />
